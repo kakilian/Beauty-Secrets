eb46f174804e716ef5a9cd58613b0b49c956d18f
Katarina Kilian <khkilian22@gmail.com>
2024-12-01 17:18:59 +0000

    Post Model with False

--- my_project/settings.py
+++ my_project/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
